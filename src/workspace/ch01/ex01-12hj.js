f38fed092d07225134485983c508c0ed633f6bcf
HyeJin003 <rkddkwnl003@gmail.com>
2025-10-10 11:12:01 +0900

    10/10일 추석 JS 복습중

---
+++ src/workspace/ch01/ex01-12hj.js
@@ -0,0 +1,12 @@
+/*
+동등 연산자와 일치 연산자 차이
+*/
+
+console.log(7 === '7'); // false
+console.log(7 == '7'); //true 
+
+console.log(null === undefined); // false
+console.log(null == undefined); //true
+
+console.log('1' === true); // false
+console.log('1' == true); //true
